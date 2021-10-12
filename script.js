b43a410d69b291b17bff5329a6aaa6cfb4f2439b
JulianaV <julianabalta16@gmail.com>
2021-10-12 09:35:00 -0400

    Some final adjustments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
   /* Page Loader */
   document.querySelector('.page-loader').classList.add('fade-out');
   setTimeout(() => {
-    document.querySelector('.page-loader').style.dispaly = 'none';
+    document.querySelector('.page-loader').style.display = 'none';
   }, 600);
 });
 
